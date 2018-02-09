f67e0207dc78df1ea6c557b1a18e76dd28ef3b36
Paul Wittle <paulwittle@users.noreply.github.com>
2018-02-09 14:47:39 +0000

    Change 2
    This has conflict

--- test.vb
+++ test.vb
@@ -1,9 +1,9 @@
-Dim Paul as String = "Hi"
+Dim PaulW3 As String = "Hi"
 
-If Paul = "Hi" Then
-	Console.WriteLine("Paul Rocks Still")
+If PaulW3 = "Hi" Then
+Console.WriteLine("Paul Rocks Still")
 End If
 
-If Paul <> "Hi" Then
-	Console.WriteLine("Paul Rocks")
+If PaulW3 <> "Hi" Then
+Console.WriteLine("Paul Rocks")
 End If
