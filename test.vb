1a95b05318dc846e59ace800efa380043330e45b
Paul Wittle <paulwittle@users.noreply.github.com>
2018-02-12 12:24:01 +0000

    Non conflicting change 2
    This should not clash with branch 2

--- test.vb
+++ test.vb
@@ -1,4 +1,4 @@
-public function Paul(byval input as boolean) as string
+public function PaulSays(byval input as boolean) as string
 	If input Then
 		return "Hi"
 	Else
@@ -6,12 +6,12 @@ public function Paul(byval input as boolean) as string
 	End If
 end function
 
-If Paul(true) = "Hi" Then
+If PaulSays(true) = "Hi" Then
 Console.WriteLine("Paul says hello")
 End If
 
-If Paul(false) <> "Hi" Then
+If PaulSays(false) <> "Hi" Then
 Console.WriteLine("Paul doesn't greet you")
 End If
 
-Console.WriteLine("Paul says " & Paul(true))
+Console.WriteLine("Paul says " & PaulSays(true))
